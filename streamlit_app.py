b94053b8935be36aefac7a6ac793d6d9ee790c44
rbb775-source <rbb775@goedu.kr>
2025-10-01 09:22:16 +0000

    kkk

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("🎈 태영이의 첫번째 앱")
+st.title("🎈 태영이의 100번째 앱")
 st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
 st.image("https://cdnweb01.wikitree.co.kr/webdata/editor/202112/06/img_20211206220519_7390a79c.webp")
