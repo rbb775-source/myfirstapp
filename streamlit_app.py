cdcc69c809cb52f188d4e1cacf6edcef07c99bca
rbb775-source <rbb775@goedu.kr>
2025-10-01 09:10:38 +0000

    첫 번째 앱 배포

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 태영이의 첫번째 앱")
+st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
+st.image("https://cdnweb01.wikitree.co.kr/webdata/editor/202112/06/img_20211206220519_7390a79c.webp")
